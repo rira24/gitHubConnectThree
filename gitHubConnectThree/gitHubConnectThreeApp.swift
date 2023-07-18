2828e454a8e9fe03be17454a19a08b622d846b49
Rithika <rithu427@gmail.com>
2023-07-18 10:02:35 -0500

    Initial Commit

---
+++ gitHubConnectThree/gitHubConnectThreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitHubConnectThreeApp.swift
+//  gitHubConnectThree
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct gitHubConnectThreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
